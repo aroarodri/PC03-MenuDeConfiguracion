ce52ff4097f4856d5973cfc1b3bb624bfd189e7c
aroarodri <aroa.rodriguez@ikasle.egibide.org>
2024-10-20 21:25:54 +0200

    Player working + PauseMenu not working

---
+++ Assets/Code/PauseMenu.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private TextMeshProUGUI speedText;
+    [SerializeField] private Slider slider;
+    [SerializeField] private Player player;
+
+    private bool isPaused = false;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        if (isPaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    private void Pause()
+    {
+        isPaused = true;
+        // Pausa el tiempo
+        Time.timeScale = 0f;
+
+        pauseMenu.SetActive(true);
+    }
+
+    private void Resume()
+    {
+        isPaused = false;
+
+        Time.timeScale = 1f;
+
+        pauseMenu.SetActive(false);
+    }
+}
--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float upForce = 250f;
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody rb;
+    private Vector2 move;
+
+    private CutomActionsController cutomActionsController;
+    private PauseMenu pauseMenu;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        cutomActionsController = new CutomActionsController();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(move.x, 0, move.y) * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
+    }
+
+    private void OnEnable()
+    {
+        cutomActionsController.Player.Enable();
+        cutomActionsController.Player.Jump.performed += OnJump;
+        cutomActionsController.Player.Move.performed += OnMove;
+        cutomActionsController.Player.Move.canceled += OnMove;
+
+        cutomActionsController.PauseMenu.Enable();
+        cutomActionsController.PauseMenu.Pause.performed += pauseMenu.OnPause;
+    }
+
+
+    public void OnJump(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.performed)
+        {
+            rb.AddForce(Vector3.up * upForce);
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.performed)
+        {
+            move = callbackContext.ReadValue<Vector2>();
+        }
     }
 }
